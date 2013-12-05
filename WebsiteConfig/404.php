9b270b13e1b4e418aeb05beecc56197a3438194c
longluo <calmbody@163.com>
2013-12-05 19:59:53 +0800

    Adding the Website Config folder to save the Nginx/PHP/WP configure files. Remove the finderplugin folder.

---
+++ WebsiteConfig/404.php
@@ -0,0 +1,7 @@
+﻿<?php
+$pos = strrpos($qs, '://'); 
+$pos = strpos($qs, '/', $pos + 4);
+$_SERVER['REQUEST_URI'] = substr($qs, $pos); 
+$_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI']; 
+include('index.php'); 
+?>
