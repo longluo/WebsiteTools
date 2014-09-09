daa4ebcd931fdfbd9dbcf5cba13ed702ee081e83
longluo <calmbody@163.com>
2014-09-10 01:15:21 +0800

    Update the weather icons and some images resources.

---
+++ Web/weather/js/weather.js
@@ -0,0 +1,19 @@
+// Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+    $.simpleWeather({
+        location: 'Shenzhen, China',
+        woeid: '2161853',
+        unit: 'c',
+        success: function(weather) {
+            html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+            html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+            html += '<li class="currently">'+weather.currently+'</li>';
+            html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+
+            $("#weather").html(html);
+        },
+        error: function(error) {
+            $("#weather").html('<p>'+error+'</p>');
+        }
+    });
+});
